050aa33e19c9ec30df982e01b6e373c24178d866
Dan Reich <dancreich@gmail.com>
2017-06-02 08:58:51 -0400

    remap sudoku board to grid coord system.

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -5,49 +5,31 @@ import Square from './Square.js';
 export default class Sudoku extends React.Component {
   render () {
     const board = this.props.board;
-    const subSquares = [];
-    for (let squareRow = 0; squareRow < board.length / 3; squareRow++) {
-      for (let squareCol = 0; squareCol < board[0].length / 3; squareCol++) {
-
-        const square = [];
-        const subRowStart = squareRow * 3;
-        const subRowEnd = subRowStart + 3
-
-        for (let subRow = subRowStart; subRow < subRowEnd; subRow++) {
-          const subColStart = squareCol * 3;
-          const subColEnd = subColStart + 3;
-
-          for (let subCol = subColStart; subCol < subColEnd; subCol++) {
-            square.push(board[subRow][subCol]);
-          }
-        }
-
-        subSquares.push(square);
+    const squares = [];
+    for (const key in board) {
+      if (key) {
+        squares.push(
+          <Square
+            key={key}
+            handleChange={this.props.handleChange.bind(this, key)}
+            value={board[key]} />
+        );
       }
     }
 
     return (
       <div className="sudoku-board">
-        {subSquares.map((subSquare, index) => {
-          return (
-            <div key={index} className="sub-square">
-              {subSquare.map((square) => {
-                return (
-                  <Square
-                    key={square.loc}
-                    handleChange={this.props.handleChange.bind(this, square.loc)}
-                    value={square.value} />
-                );
-              })}
-            </div>
-          );
-        })}
+        <span className="cross-line top"></span>
+        <span className="cross-line bottom"></span>
+        <span className="cross-line left"></span>
+        <span className="cross-line right"></span>
+        {squares}
       </div>
     );
   }
 }
 
 Sudoku.propTypes = {
-  board: PropTypes.array.isRequired,
+  board: PropTypes.object.isRequired,
   handleChange: PropTypes.func.isRequired,
 };
--- src/SudokuContainer.js
+++ src/SudokuContainer.js
@@ -4,28 +4,86 @@ import Sudoku from './Sudoku.js';
 const BOARD_HEIGHT = 9;
 const BOARD_LENGTH = 9;
 
+const _cross = (a, b) => {
+  const crossResult = [];
+
+  a.forEach((aEl) => {
+    b.forEach((bEl) => {
+      crossResult.push(aEl + bEl)
+    });
+  });
+
+  return crossResult;
+}
+
+const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+const cols = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const BOXES = _cross(rows, cols);
+const ROW_UNITS = [];
+const COL_UNITS = [];
+const SQUARE_UNITS = [];
+const squareRowSets = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']];
+const squareColSets = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+const UNITS = {};
+const PEERS = {};
+
+// Create row units array
+rows.forEach((row) => {
+  ROW_UNITS.push(_cross([row], cols));
+});
+
+// Create col units array
+cols.forEach((col) => {
+  COL_UNITS.push(_cross(rows, [col]));
+});
+
+// Create square units array
+squareRowSets.forEach((rowSet) => {
+  squareColSets.forEach((colSet) => {
+    SQUARE_UNITS.push(_cross(rowSet, colSet));
+  });
+});
+
+// Create array of all units combined
+const ALL_UNITS = ROW_UNITS.concat(COL_UNITS).concat(SQUARE_UNITS);
+
+// Create a js object where the key is a boxes' location
+// and the value is an array of the units in which is box is contained.
+// Populate the peers object with keys for each box and values for all
+// a boxes' unique peers
+BOXES.forEach((box) => {
+  UNITS[box] = [];
+  PEERS[box] = [];
+
+  ALL_UNITS.forEach((unit) => {
+    if (unit.includes(box)) {
+      UNITS[box].push(unit);
+    }
+  });
+
+  UNITS[box].forEach((unit) => {
+    unit.forEach((peer) => {
+      if (!PEERS[box].includes(peer)) {
+        PEERS[box].push(peer);
+      }
+    });
+  });
+});
+
 export default class SudokuContainer extends React.Component {
   constructor() {
     super();
     this.setState = this.setState.bind(this);
 
-    const board = [];
+    const board = {};
+    const solution = {};
 
-    for (let row = 0; row < BOARD_LENGTH; row++) {
-      const columns = [];
+    BOXES.forEach((box) => {
+      board[box] = '';
+      solution[box] = '';
+    });
 
-      for (let col = 0; col < BOARD_HEIGHT; col++) {
-        const square = {
-          loc: `${row}${col}`,
-          value: '',
-        };
-
-        columns.push(square);
-      }
-      board.push(columns);
-    }
-
-    this.state = {board,};
+    this.state = {board, solution, solving: false, };
   }
 
   handleChange (loc, e) {
@@ -33,9 +91,12 @@ export default class SudokuContainer extends React.Component {
     if (value && (value < 1 || value > 9)) return;
 
     const board = this.state.board;
-    board[loc[0]][loc[1]].value = e.target.value;
+    const solution = this.state.solution;
 
-    this.setState(board);
+    board[loc] = e.target.value;
+    solution[loc] = e.target.value;
+
+    this.setState({board, solution,});
   }
 
   render() {

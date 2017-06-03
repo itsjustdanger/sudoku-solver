0594e7f5d82b90fa4fe7567ce0bd988d6519b4de
Dan Reich <dancreich@gmail.com>
2017-06-03 12:51:28 -0400

    remove some default create-react-app-stuff.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import SudokuContainer from './SudokuContainer.js';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
